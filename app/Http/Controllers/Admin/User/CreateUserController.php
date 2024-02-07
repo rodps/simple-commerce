ef2c18f2d4384af30c8a1420c27d305ae775f5b2
Rodrigo da Silva <rodrigopds73@gmail.com>
2024-02-07 05:24:26 -0300

    refactor: remove user service

--- app/Http/Controllers/Admin/User/CreateUserController.php
+++ app/Http/Controllers/Admin/User/CreateUserController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers\Admin\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\CreateUserRequest;
 use App\Http\Services\UserService;
+use App\Models\User;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 class CreateUserController extends Controller
 {
-    public function __construct(private UserService $userService) {}
-
     public function __invoke(CreateUserRequest $request)
     {
         $data = $request->validated();
 
-        $response = $this->userService->create($data);
+        $userAlreadyExists = User::where('email', $data['email'])->first();
 
-        return response()->json($response, 201);
+        if ($userAlreadyExists) {
+            throw new ConflictHttpException('User already exists');
+        }
+
+        $user = User::create(array_merge($data, ['role' => 'ADMIN']));
+
+        return response()->json($user, 201);
     }
 }
